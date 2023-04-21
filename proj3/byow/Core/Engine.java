0643d9661f08c593f81dbf3e3e427a81963fa505
Pari <ppandya469@berkeley.edu>
2023-04-21 11:22:56 -0700

    no changes

--- proj3/byow/Core/Engine.java
+++ proj3/byow/Core/Engine.java
@@ -3,11 +3,11 @@ package byow.Core;
 import byow.TileEngine.TERenderer;
 import byow.TileEngine.TETile;
 import edu.princeton.cs.algs4.StdDraw;
-import org.junit.Test;
+//import org.junit.Test;
 
-import java.awt.*;
+//import java.awt.*;
 
-import static com.google.common.truth.Truth.assertThat;
+//import static com.google.common.truth.Truth.assertThat;
 
 public class Engine {
     TERenderer ter = new TERenderer();
@@ -36,22 +36,18 @@ public class Engine {
 
     /**
      * Method used for autograding and testing your code. The input string will be a series
-     * of characters (for example, "n123sswwdasdassadwas", "n123sss:q", "lwww". The engine should
+     * of characters (for example, "n123sswwdasdassadwas", "n123sss:q", "lwww"). The engine should
      * behave exactly as if the user typed these characters into the engine using
      * interactWithKeyboard.
-     *
      * Recall that strings ending in ":q" should cause the game to quite save. For example,
      * if we do interactWithInputString("n123sss:q"), we expect the game to run the first
      * 7 commands (n123sss) and then quit and save. If we then do
      * interactWithInputString("l"), we should be back in the exact same state.
-     *
      * In other words, running both of these:
      *   - interactWithInputString("n123sss:q")
      *   - interactWithInputString("lww")
-     *
      * should yield the exact same world state as:
      *   - interactWithInputString("n123sssww")
-     *
      * @param input the input string to feed to your program
      * @return the 2D TETile[][] representing the state of the world
      */
@@ -59,9 +55,8 @@ public class Engine {
         // passed in as an argument, and return a 2D tile representation of the
         // world that would have been drawn if the same inputs had been given
         // to interactWithKeyboard().
-        //
         // See proj3.byow.InputDemo for a demo of how you can make a nice clean interface
-        // that works for many different input types.
+        // that works for different input types.
         char[] c = input.toCharArray();
         Array a = new Array(WIDTH, HEIGHT);
         for (int i = 0; i < c.length; i++) {
